d4b4150ff7339fc5b6647313e1029ae13d22ed73
Adi-D-king <as4497119@gmail.com>
2024-02-02 18:22:13 +0530

    updateed at line 24

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
-import MySQLdb as db
+import MySQLdb as db 
 print('*************************************** ',end="")
 print('Wellcome to inventeri managment system.',end="")
 print(' ***************************************')
 
 #connecting database
-conn = db.connect(host = 'localhost', database= 'testing', user = 'root', password = 'Adity@4410')
+conn =db.connect(user='root',password='Adity@4410',host='localhost',database='testing')
 cursor = conn.cursor() # creating cursor
-cursor.execute("select * from inventry;")
+cursor.execute("use testing;")
+print("using testing!")
 
 #user options 
 print('''Plese enter choice.
 1. Enter new Item.
-2. View Inventeri 
+2. View Inventry
 0. Exit.''')
 choice = int(input('Enter choice :>'))  #user input
 
 if choice == 1:
     Sir_no = int(input("Sr_no:"))
-    P_name = input("Product_name:")
-    P_type = input('Type:')
-    Price = float(input('Price:'))
-    sql = "INSERT INTO Inventry (Sir_no, Product_name, Type, Price) VALUES (%i, %s, %s, %f)".format(Sir_no,P_name,P_type,Price)
-    val = (Sir_no,P_name,P_type,Price)
+    Product_name = "Name"
+    Type = "Unknown"
+    Price = 10.5
+    sql = "insert into inventry (Sir_no, Product_name, Type, Price) value ({},'{}','{}',{});".format(Sir_no,Product_name,Type,Price)
     cursor.execute(sql)
+    print("data entered")
+    cursor.commit()
 
 elif choice == 2:
+    cursor.execute("select * from inventry")
     #row = cursor.fetchone() #get onr row
     rows = cursor.fetchall() #get all rows
     for i in rows:
