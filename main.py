6015c7a7ce09e9b37d57109f445a80af6d013f7e
Adi-D-king <as4497119@gmail.com>
2024-02-03 14:36:15 +0530

    new update

--- main.py
+++ main.py
@@ -11,12 +11,26 @@ print("using testing!")
 
 #user options 
 print('''Plese enter choice.
-1. Enter new Item.
-2. View Inventry
+1. Bill Genration
+2. Enter new Item.
+3. View Inventry
 0. Exit.''')
 choice = int(input('Enter choice :>'))  #user input
 
 if choice == 1:
+    itme_list = []
+    quantity = []
+    O ='y'
+    while O == 'y':
+        itme_list.append(str(input("Enter product name:")))
+        quantity.append(int(input("Enter quantitya:")))
+        O = input("y= next_item/n= exit :")
+    for i in itme_list:
+        sql = "select (Price) from inventry where {};".format(i)
+        cursor.execute(sql)
+        print(cursor.fetchone()*quantity[0])
+
+elif choice == 2:
     Sir_no = int(input("Sr_no:"))
     Product_name = input("Enter name Product:")
     Type = input("Enter type of product:")
@@ -24,9 +38,9 @@ if choice == 1:
     sql = "insert into inventry (Sir_no, Product_name, Type, Price) value ({},'{}','{}',{});".format(Sir_no,Product_name,Type,Price)
     cursor.execute(sql)
     print("data entered")
-    cursor.commit()
+    conn.commit()
 
-elif choice == 2:
+elif choice == 3:
     cursor.execute("select * from inventry")
     #row = cursor.fetchone() #get onr row
     rows = cursor.fetchall() #get all rows
